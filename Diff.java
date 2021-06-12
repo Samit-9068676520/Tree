623664dc971a4c5059ac1fdf341c1642433f9f57
Samit-9068676520 <samitverma279@gmail.com>
2021-06-12 18:48:18 +0530

    added implementyation Diff.java

--- Diff.java
+++ Diff.java
@@ -1,3 +1,8 @@
 public class Diff{
-    
+    public static void main(String args[])
+    {
+        int a=9;
+        int b=12;
+        System.out.println(b-a);
+    }
 }
--- Multiply.java
+++ Multiply.java
@@ -1,6 +1,6 @@
 public class Multiply{
     public static void main(String args[])
-    {
+
         int a=3;
         int b=9;
         System.out.println(a*b);
